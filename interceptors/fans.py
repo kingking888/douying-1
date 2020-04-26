1adfa69c9d66368695ea336cd6b06bb23ea9f6c8
shangmw <shangmw>
2020-04-26 18:50:33 +0800

    douying fans

--- interceptors/fans.py
+++ interceptors/fans.py
@@ -3,21 +3,26 @@
 '''
 import json
 from interceptors.interceptor import Interceptor
+from handle_db import mongo_info
 
 class FansInterceptor(Interceptor):
     def __init__(self):
         Interceptor.__init__(self,'aweme/v1/user/follower/list/')
 
     def handle(self,flow):
-        print("*************************************************")
         for user in json.loads(flow.response.text)['followers']:
+            short_id = user['short_id']
             user_info = {}
             user_info['share_id'] = user['uid']
-            user_info['douyin_id'] = user['short_id']
+            user_info['douyin_id'] = short_id
+            user_info['unique_id'] = user['unique_id']
+            # if not short_id or short_id == '0' :
+            #     user_info['douyin_id'] = user['unique_id']
+
             user_info['nickname'] = user['nickname']
             # 0 male 1 female 2 unset
             user_info['gender'] = user['gender']
             user_info['birthday'] = user['birthday']
             user_info['status'] = user['status']
             user_info['region'] = user['region']
-            print(user_info)
+            mongo_info.save_task(user_info)
